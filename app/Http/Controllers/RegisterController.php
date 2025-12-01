3a7774538d3adc0d9d107d7a3d047c01630482b0
frenky <frenky22@student.uisi.ac.id>
2025-12-01 13:11:10 +0700

    few fix

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -14,23 +14,18 @@ class RegisterController extends Controller
     }
     public function store(Request $request)
     {
-        // 1. Validasi Input
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users', // Pastikan email unik di tabel users
-            'password' => 'required|string|min:8|confirmed', // 'confirmed' akan mengecek input name="password_confirmation"
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
         ]);
 
-        // 2. Simpan ke Database
-        // Kita menggunakan Eloquent Model 'User'
         User::create([
             'name' => $request->name,
             'email' => $request->email,
-            // Password WAJIB di-hash (enkripsi) agar aman
             'password' => Hash::make($request->password),
         ]);
 
-        // 3. Redirect setelah sukses
         return redirect('/login')->with('success', 'Registrasi berhasil! Silakan login.');
     }
 }
