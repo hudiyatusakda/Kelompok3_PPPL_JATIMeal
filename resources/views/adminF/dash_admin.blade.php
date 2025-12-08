d04c1789d20b4857033a794d4cbf2148be0e279b
frenky <frenky22@student.uisi.ac.id>
2025-12-08 12:23:29 +0700

    dashboard admin half design

--- resources/views/adminF/dash_admin.blade.php
+++ resources/views/adminF/dash_admin.blade.php
@@ -1,13 +1,145 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="dash_admin.css">
-    <title>Document</title>
+
+    {{-- Link Font dirapikan --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:wght@100..900&family=SUSE:wght@100..800&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('css/dash_admin.css') }}">
+
+    <title>Home Admin - MealGoal</title>
 </head>
+
 <body>
-    
+    <main>
+        <div class="container">
+            <div class="left-section">
+                <div class="logo_placeholder">
+                    <div class="logo">
+                        <img src="{{ asset('img/JatimMeal.png') }}" alt="JatimMeal">
+                    </div>
+                </div>
+                <div class="side-bar-menu">
+                    <div class="side-bar">
+                        <ul>
+                            <li class="list active"><a href="#">Tambahkan Menu</a></li>
+                            <li class="list"><a href="#">List Menu</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="right-section">
+                <div class="navbar">
+                    {{-- Kalau sudah Login --}}
+                    <div class="navbar-user">
+                        <div class="profile-dropdown">
+                            <div class="profile-trigger" onclick="toggleMenu()">
+                                <span class="user-name">{{ Auth::user()->name ?? 'User' }}</span>
+                                <div class="account">
+                                    <img src="{{ asset('img/Tester.jpg') }}" alt="Profile">
+                                </div>
+                                <i class="fa-solid fa-caret-down"></i>
+                            </div>
+
+                            <div class="dropdown-content" id="subMenu">
+                                <a href="#" class="sub-item">
+                                    <i class="fa-solid fa-user"></i> Profil Saya
+                                </a>
+                                <a href="#" class="sub-item">
+                                    <i class="fa-solid fa-gear"></i> Pengaturan
+                                </a>
+                                <hr>
+                                <form action="{{ route('logout') }}" method="POST" style="padding: 0; margin: 0;">
+                                    @csrf
+                                    <button type="submit" class="sub-item logout-btn">
+                                        <i class="fa-solid fa-right-from-bracket"></i> Keluar
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="content">
+                    <div class="input-menu">
+                        <label for="menu">Nama Menu:</label>
+                        <input type="text" id="menu">
+                    </div>
+                    <div class="insert-image">
+                        <input type="file" id="file-input" accept="image/*">
+
+                        <label for="file-input" id="drop-area">
+                            <svg id="upload-icon" xmlns="http://www.w3.org/2000/svg" width="80" height="80"
+                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                                <polyline points="17 8 12 3 7 8"></polyline>
+                                <line x1="12" y1="3" x2="12" y2="15"></line>
+                            </svg>
+
+                            <img id="img-preview" src="#" alt="Preview">
+                        </label>
+                    </div>
+                    <div class="deskripsi-field">
+                        <div class="label-deskripsi">Deskripsi Menu:</div>
+                        <div class="input-deskripsi">
+                            <input type="text" name="" id="">
+                        </div>
+                    </div>
+                    <button type="submit">Tambahkan Menu</button>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <script src="https://kit.fontawesome.com/6306b536ce.js" crossorigin="anonymous"></script>
+
+    <script>
+        let subMenu = document.getElementById("subMenu");
+
+        function toggleMenu() {
+            subMenu.classList.toggle("open-menu");
+        }
+
+        window.onclick = function(event) {
+            if (!event.target.closest('.profile-dropdown')) {
+                if (subMenu && subMenu.classList.contains('open-menu')) {
+                    subMenu.classList.remove('open-menu');
+                }
+            }
+        }
+
+        const fileInput = document.getElementById('file-input');
+        const imgPreview = document.getElementById('img-preview');
+        const uploadIcon = document.getElementById('upload-icon');
+
+        fileInput.addEventListener('change', function(event) {
+            const file = event.target.files[0];
+
+            if (file) {
+                const reader = new FileReader();
+
+                reader.onload = function(e) {
+                    imgPreview.src = e.target.result;
+
+                    imgPreview.style.display = 'block';
+
+                    uploadIcon.style.display = 'none';
+                }
+
+                reader.readAsDataURL(file);
+            }
+        });
+    </script>
 </body>
+
 </html>
