600bfde23ceeae698c716dc6ef89d01b52e03290
frenky <frenky22@student.uisi.ac.id>
2026-01-14 13:10:45 +0700

    fix another profile navbar

--- resources/views/adminF/user_detail.blade.php
+++ resources/views/adminF/user_detail.blade.php
@@ -66,17 +66,26 @@
 
         <div class="right-section">
             <div class="navbar">
+                {{-- Kalau sudah Login --}}
                 <div class="navbar-user">
                     <div class="profile-dropdown">
                         <div class="profile-trigger" onclick="toggleMenu()">
-                            <span class="user-name">{{ Auth::user()->name ?? 'Admin' }}</span>
+                            <span class="user-name">{{ Auth::user()->name ?? 'User' }}</span>
                             <div class="account">
-                                <img src="https://placehold.co/50x50" alt="Profile">
+                                <img src="{{ asset('img/Tester.jpg') }}" alt="Profile">
                             </div>
                             <i class="fa-solid fa-caret-down"></i>
                         </div>
+
                         <div class="dropdown-content" id="subMenu">
-                            <form action="{{ route('logout') }}" method="POST">
+                            <a href="#" class="sub-item">
+                                <i class="fa-solid fa-user"></i> Profil Saya
+                            </a>
+                            <a href="#" class="sub-item">
+                                <i class="fa-solid fa-gear"></i> Pengaturan
+                            </a>
+                            <hr>
+                            <form action="{{ route('logout') }}" method="POST" style="padding: 0; margin: 0;">
                                 @csrf
                                 <button type="submit" class="sub-item logout-btn">
                                     <i class="fa-solid fa-right-from-bracket"></i> Keluar
@@ -209,7 +218,8 @@
                                                         </div>
                                                     @else
                                                         {{-- Tampilan Kosong --}}
-                                                        <span style="color: #bbb; font-style: italic; font-size: 13px;">
+                                                        <span
+                                                            style="color: #bbb; font-style: italic; font-size: 13px;">
                                                             - Tidak ada jadwal -
                                                         </span>
                                                     @endif
